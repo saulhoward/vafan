44f850a76368b995294bbb0dbd96fb79dc3728b8
Saul Howard <saul@vafan.(none)>
2011-08-14 02:38:37 +0100

    added static files

---
+++ scripts/goinstall.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+###
+# Goinstall script for packages used in Vafan server
+#
+# @url    http://github.com/saulhoward/vafan 
+# @author Saul <saul@saulhoward.com>
+###
+
+# Web.go -hoise +fiber
+goinstall -v github.com/fiber/web.go
+
+# goconfig
+goinstall -v github.com/kless/goconfig/config
+
+# mustache.go
+goinstall -v github.com/hoisie/mustache.go
